79708a95a0fef8ded557a90bc41ff0d5d09baf81
Aryan <aryan01001s@gmail.com>
2026-06-10 12:40:20 +0530

    build pipeline

---
+++ src/content/index.js
@@ -0,0 +1,1 @@
+console.log('LeetSense content loaded');
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+require('dotenv').config();
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-source-map',
+  entry: {
+    content: './src/content/index.js',
+    background: './src/background/index.js',
+    popup: './src/popup/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '.' },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      'process.env.GROQ_API_KEY': JSON.stringify(process.env.GROQ_API_KEY),
+    }),
+  ],
+};
